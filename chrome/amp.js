66fdb5707d35bfb4c2b7ed55f1ed90ee41db1289
edlea <ed@edlea.net>
2015-10-21 14:51:00 +0100

    Run on content_start to load quicker

--- chrome/amp.js
+++ chrome/amp.js
@@ -1,4 +1,24 @@
-var amp = document.head.querySelector("link[rel=amphtml]");
-  if (amp && amp.getAttribute('href')) {
-      window.location = amp.getAttribute('href');
-  }
+var isAMP = (document.documentElement.getAttribute('amp') === "" || document.documentElement.getAttribute('⚡️') === "");
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    var length = mutation.addedNodes.length;
+    for (var i = 0; i < length; i++) {
+      var node = mutation.addedNodes[i];
+      if (node.tagName == "LINK") {
+        if (node.getAttribute('rel').toLowerCase() == 'amphtml') {
+            window.location = node.getAttribute("href");
+        }
+      }
+      else if (isAMP && node.tagName == "HEAD") {
+        var css = "body > * { max-width: 600px; margin: 0px auto; }";
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(css));
+        node.appendChild(style);
+      }
+    }
+  });
+});
+
+var config = { attributes: true, childList: true, subtree: true };
+observer.observe(document.documentElement, config);
