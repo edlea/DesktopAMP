7c8f55d367e3eb60ed2802e4939372d4ec079fca
Matttter <Matttter@users.noreply.github.com>
2016-10-03 16:20:44 +0200

    Update background.js

--- src/background.js
+++ src/background.js
@@ -19,11 +19,11 @@ var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   toggle = !toggle;
   if(toggle){
-    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, {file:"SCRIPT.user.js"});
+    chrome.browserAction.setIcon({path: "icons/Icon-128.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"amp.js"});
   }
   else{
-    chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
+    chrome.browserAction.setIcon({path: "icons/Icon-128-inactive.png", tabId:tab.id});
     chrome.tabs.executeScript(tab.id, {code:"alert()"});
   }
 });
