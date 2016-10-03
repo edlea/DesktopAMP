91cd4312b8edd58d9bec692c8fbe653c3eaf13af
Matttter <Matttter@users.noreply.github.com>
2016-10-03 16:08:01 +0200

    Off toggle for extension initial commit

--- src/background.js
+++ src/background.js
@@ -15,3 +15,15 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  if(toggle){
+    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"SCRIPT.user.js"});
+  }
+  else{
+    chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {code:"alert()"});
+  }
+});
